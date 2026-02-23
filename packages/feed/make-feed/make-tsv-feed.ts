60d67a0ae82e10c6984a73f2b7afb2ea6ce97d24
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2026-02-23 19:37:59 +0100

    fix date format

--- packages/feed/make-feed/make-tsv-feed.ts
+++ packages/feed/make-feed/make-tsv-feed.ts
@@ -1,10 +1,12 @@
 import type { EpisodeWatchedDate } from '../shared/types';
 
+const pad = (num: Number) => (String(num).length == 1 ? `0${num}` : num);
+
 const getItemDate = (episode: EpisodeWatchedDate): string => {
   const year = episode.date.getFullYear();
   const month = episode.date.getMonth() + 1;
   const day = episode.date.getDate();
-  return `${year}-${month}-${day}`;
+  return `${year}-${pad(month)}-${pad(day)}`;
 };
 
 export const makeTsvFeed = (episodes: EpisodeWatchedDate[]): string => {
