2dd9bf6241c1225ae0f5dcd177ecd8f7bd70e1f2
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2026-03-27 12:04:29 +0100

    remove code for making feed v1

--- packages/feed/make-feed/types.ts
+++ packages/feed/make-feed/types.ts
@@ -1,15 +1,3 @@
-export type JsonFeed = {
-  title: string;
-  lastBuildDate: Date;
-  items: JsonFeedItem[];
-};
-
-export type JsonFeedItem = {
-  title: string;
-  episodeTitle: string;
-  pubDate: Date;
-};
-
 export type JsonFeedV2 = {
   buildDate: Date;
   lastWatchedEpisode: {
