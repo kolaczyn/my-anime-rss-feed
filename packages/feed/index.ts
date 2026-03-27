4e5b4b63390e1e212e7595ab5469cf0d10df60ae
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2026-03-27 11:05:34 +0100

    organize files

--- packages/feed/index.ts
+++ packages/feed/index.ts
@@ -1,53 +1,11 @@
+import { fetchAndExtractWatchedDates } from './make-feed/fetch-and-extract-watched-dates.ts';
 import { makeJsonFeed } from './make-feed/make-json-feed.ts';
-import narutoDb from './jikan-api/anime-20-cached.json';
-import shippudenDb from './jikan-api/anime-1735-cached.json';
 import { makeTsvFeed } from './make-feed/make-tsv-feed.ts';
-import { fetchWatchedDates } from './mal-api/mal-api-watched-dates.ts';
-import { extractWatchedDates } from './mal-api/mal-extract-watched-dates/mal-extract-watched-dates';
 import { narutoId, shippuudenId } from './shared/config.ts';
 import { uploadFile } from './upload-file/upload-file.ts';
-import type { EpisodeData, EpisodeFullData } from './shared/types.ts';
 
 // This fixes timezone issues in Github actions
 process.env.TZ = 'Europe/Warsaw';
-
-const getAnimeData = (animeId: number) => {
-  // this is not very generic, but whatever :p
-  const dbToUse = animeId === 20 ? narutoDb : shippudenDb;
-  const hashMap: Record<number, EpisodeData> = dbToUse.reduce(
-    (acc, curr) => {
-      acc[curr.mal_id] = { filler: curr.filler, title: curr.title };
-      return acc;
-    },
-    {} as Record<number, EpisodeData>,
-  );
-
-  return hashMap;
-};
-
-const fetchAndExtractWatchedDates = async (
-  animeId: number,
-): Promise<EpisodeFullData[]> => {
-  const watchedDates = await fetchWatchedDates(animeId).then((html) =>
-    extractWatchedDates(html),
-  );
-
-  const animeData = getAnimeData(animeId);
-  const episodeFullData: EpisodeFullData[] = watchedDates.map((x) => {
-    const epData = animeData[x.episode];
-    if (!epData) {
-      throw new Error(
-        `Could not find data for episode ${x.episode} in anime ${animeId}`,
-      );
-    }
-    return {
-      ...x,
-      ...epData,
-    };
-  });
-
-  return episodeFullData;
-};
 
 const main = async () => {
   const [epNaruto, epShippuuden] = await Promise.all([
---
+++ packages/feed/make-feed/fetch-and-extract-watched-dates.ts
@@ -0,0 +1,45 @@
+import { fetchWatchedDates } from '../mal-api/mal-api-watched-dates.ts';
+import { extractWatchedDates } from '../mal-api/mal-extract-watched-dates/mal-extract-watched-dates';
+import type { EpisodeFullData } from '../shared/types.ts';
+
+import shippudenDb from '../jikan-api/anime-1735-cached.json';
+import narutoDb from '../jikan-api/anime-20-cached.json';
+import type { EpisodeData } from '../shared/types.ts';
+
+const getAnimeData = (animeId: number) => {
+  // this is not very generic, but whatever :p
+  const dbToUse = animeId === 20 ? narutoDb : shippudenDb;
+  const hashMap: Record<number, EpisodeData> = dbToUse.reduce(
+    (acc, curr) => {
+      acc[curr.mal_id] = { filler: curr.filler, title: curr.title };
+      return acc;
+    },
+    {} as Record<number, EpisodeData>,
+  );
+
+  return hashMap;
+};
+
+export const fetchAndExtractWatchedDates = async (
+  animeId: number,
+): Promise<EpisodeFullData[]> => {
+  const watchedDates = await fetchWatchedDates(animeId).then((html) =>
+    extractWatchedDates(html),
+  );
+
+  const animeData = getAnimeData(animeId);
+  const episodeFullData: EpisodeFullData[] = watchedDates.map((x) => {
+    const epData = animeData[x.episode];
+    if (!epData) {
+      throw new Error(
+        `Could not find data for episode ${x.episode} in anime ${animeId}`,
+      );
+    }
+    return {
+      ...x,
+      ...epData,
+    };
+  });
+
+  return episodeFullData;
+};
